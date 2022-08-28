51b8024463cafb0b6e292811722b1bfd9ee54cb8
satriai0508 <satriai418@gmail.com>
2022-08-29 07:01:11 +0800

    seeder

--- database/seeders/LayananSeeder.php
+++ database/seeders/LayananSeeder.php
@@ -14,6 +14,22 @@ class LayananSeeder extends Seeder
      */
     public function run()
     {
-        Layanan::factory(30)->create();
+        Layanan::create([
+            'nama_toko' => 'Alby Barbershop',
+            'nama' => 'cepak polos',
+            'harga' => '5000000'
+        ]);
+
+        Layanan::create([
+            'nama_toko' => 'Putri Barbershop',
+            'nama' => 'botak 0,5',
+            'harga' => '5000000'
+        ]);
+
+        Layanan::create([
+            'nama_toko' => 'Ikko Barbershop',
+            'nama' => 'cukur bulu ketiak beruang',
+            'harga' => '50000000'
+        ]);
     }
 }
