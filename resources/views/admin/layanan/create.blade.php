93ca7bf6e9739d7e06aef28c4aaa9d59017d125e
satriai0508 <satriai418@gmail.com>
2022-09-18 20:51:26 +0800

    fix layanan

--- resources/views/admin/layanan/create.blade.php
+++ resources/views/admin/layanan/create.blade.php
@@ -7,7 +7,19 @@
       @csrf
       <div class="card-body">
         <h4 class="card-title d-flex justify-content-center">Create Data</h4>
-        <input type="hidden" name="nama_toko" value="{{ auth()->user()->name }}">
+        <div class="form-group row mb-2">
+          <label for="nama_toko" class="col-sm-3 text-end control-label col-form-label">Nama Usaha</label>
+          <div class="col-md-6">
+            <input type="text" class="form-control form-control-plaintext @error('nama_toko')
+                                is-invalid
+                            @enderror" id="nama_toko" name="nama_toko" placeholder="Enter Nama Toko" required value="{{ old('nama_toko') }}" autofocus />
+            @error('nama_toko')
+            <div class="invalid-feedback">
+              {{ $message }}
+            </div>
+            @enderror
+          </div>
+        </div>
         <div class="form-group row mb-2">
           <label for="nama" class="col-sm-3 text-end control-label col-form-label">Jenis Pemotongan
             Rambut</label>
@@ -25,21 +37,21 @@
           <div class="col-md-6">
             <input type="text" class="form-control form-control-plaintext @error('harga') is-invalid @enderror" id="harga" name="harga" placeholder="Enter Prices (RP)" required value="{{ old('harga') }}" />
             @error('harga')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="card-body mb-lg-2">
-                <div class="card-body d-flex justify-content-center mt-2">
-                    <a href="{{ url()->previous() }}" class="btn btn-info me-1"><i data-feather="arrow-left"></i></a>
-                    <button type="submit" class="btn btn-success">
-                    <i data-feather="file-plus"></i>
-                    </button>
-                </div>
-                </div>
-            </form>
+            <div class="invalid-feedback">
+              {{ $message }}
+            </div>
+            @enderror
+          </div>
         </div>
-    </div>
+        <div class="card-body mb-lg-2">
+          <div class="card-body d-flex justify-content-center mt-2">
+            <a href="{{ url()->previous() }}" class="btn btn-info me-1"><i data-feather="arrow-left"></i></a>
+            <button type="submit" class="btn btn-success">
+              <i data-feather="file-plus"></i>
+            </button>
+          </div>
+        </div>
+    </form>
+  </div>
+</div>
 @endsection
